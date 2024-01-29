d2c49c32d2ba46bc9e2d5dc5796ab095c786e97d
Emilson Filho <emilsonfilhocontato@gmail.com>
2024-01-29 15:50:01 -0300

    adicionando soft deletes na migration

--- database/migrations/2024_01_29_183626_create_comments_table.php
+++ database/migrations/2024_01_29_183626_create_comments_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('content');
             $table->integer('rating');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 

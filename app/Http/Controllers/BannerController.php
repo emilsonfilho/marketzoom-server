3cc213d1704aeb20b29535085d840d7800754277
Emilson Filho <emilsonfilhocontato@gmail.com>
2024-02-01 14:53:01 -0300

    feat: adding banner feature

---
+++ app/Http/Controllers/BannerController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBannerRequest;
+use App\Http\Resources\BannerResource;
+use App\Models\Banner;
+use Illuminate\Support\Facades\Storage;
+use Knuckles\Scribe\Attributes\Group;
+use Symfony\Component\HttpFoundation\Response;
+
+#[Group(name: 'Banners', description: 'Gestão dos banners do sistema')]
+class BannerController extends Controller
+{
+    /**
+     * GET api/banners
+     *
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(BannerResource::collection(Banner::all()));
+    }
+
+    /**
+     * POST api/banners
+     *
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBannerRequest $request)
+    {
+        $data = $request->validated();
+
+        $data['image'] = Storage::disk('public')->put('banners', $data['image']);
+
+        $result = Banner::create($data);
+
+        return response()->json(new BannerResource($result));
+    }
+
+    /**
+     * DELETE api/banners{banner}
+     *
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Banner $banner)
+    {
+        $banner->delete();
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
+}
